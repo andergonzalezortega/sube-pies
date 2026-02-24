7d56c3d400adc342dd28c08a1f2049eebe9104fb
andergonzalezortega <andergonzalezortega@gmail.com>
2026-02-24 10:12:14 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'subepies-v2';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(
+      ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        if (r.ok) {
+          const rc = r.clone();
+          caches.open(CACHE).then(c => c.put(e.request, rc));
+        }
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
